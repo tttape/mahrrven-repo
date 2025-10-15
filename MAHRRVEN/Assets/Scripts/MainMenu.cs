6e6ec4b889a2aa939c8478f41fe75c4cff046c8b
tttape <146393642+tttape@users.noreply.github.com>
2025-10-14 20:57:28 -0400

    made functional menu buttons

---
+++ MAHRRVEN/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        //SceneManager.LoadScene("City");
+
+        Debug.Log("Scene not setup in build yet");
+
+        //builds next scene when pressing play
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options not implemented yet");
+    }
+
+    public void QuitGame()
+    {
+
+        //for actual game build
+        //if (UNITY_EDITOR)
+        //{
+        //    EditorApplication.isPlaying = false;
+        //}
+        //else
+        //{
+        //    Application.Quit();
+        //    Debug.Log("Game has been quit");
+        //}
+
+        //for in engine testing
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+                Debug.Log("Game has been quit");
+        #endif
+
+    }
+}
