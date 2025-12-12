50753185ae7685b4a2928a4f40022fda6584a5a0
tttape <146393642+tttape@users.noreply.github.com>
2025-12-11 21:59:04 -0500

    bedroom

---
+++ MAHRRVEN/Assets/Scripts/HouseDoor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseDoor : MonoBehaviour
+{
+    [Header("Scene to Load")]
+    public string sceneToLoad = "City"; // Change to your shop scene name
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered doorway trigger!");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
