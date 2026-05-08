80c99138e58f2d94f4ed56def7f7cb217b452009
tttape <146393642+tttape@users.noreply.github.com>
2026-05-08 10:00:13 -0400

    music

---
+++ MAHRRVEN/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(gameObject);
+            created = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
